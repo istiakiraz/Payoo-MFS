f64ef8cfa181e8725fed8ae9877e4f1b7cdd466e
istiakiraz <istiakiraz@gmail.com>
2025-02-27 21:34:41 +0600

    login page

---
+++ js/login.js
@@ -0,0 +1,23 @@
+document.getElementById('login-btn').addEventListener("click", function(event){
+    event.preventDefault();
+
+    const acNumber = document.getElementById('ac-number').value;
+    
+    const acPin = document.getElementById('ac-pin').value;
+    const convetPin = parseInt(acPin)
+
+   if (acNumber === "01301301301"){ //  issues 01
+    if(convetPin===1234){
+        window.location.href="./main.html"
+
+    }
+    else{
+        alert("Your Pin Incorrect")
+    }
+   }
+   else{
+    alert("Need a valid AC Number");
+   }
+
+    
+})
